0def60b565061ca8df4755882cf027aa549cc64f
Gasparsk <143566536+Gasparsk@users.noreply.github.com>
2024-08-20 15:22:52 -0300

    Create AT04.java

---
+++ AT04.java
@@ -0,0 +1,49 @@
+:/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject4;
+
+import java.util.Scanner;
+
+
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    Scanner sc = new Scanner(System.in);
+     int numero = 0;
+     int par = 0;
+     int impar = 0;
+     int num = 0;
+     
+     do{
+     
+         System.out.printf("DIGITE UM NUMERO:");
+         numero = sc.nextInt();
+         
+         if (numero % 2 == 0 && numero >= 0){
+         
+             par++;
+         
+         }
+         
+         else if (numero % 2 == 1 && numero >= 0){
+         
+             impar++;
+         
+         }
+         num++;
+   
+     } while (numero >= 0);
+     
+     num--;
+     
+     System.out.println("QUANTIDADE DE NUMEROS PARES:" + par);
+     System.out.println("QUANTIDADE DE NUMEROS IMPARES:" + impar);
+     System.out.println("QUANTIDADE DE NUMEROS INSERIDOS:" + num);
+     
+     
+    }
+}
