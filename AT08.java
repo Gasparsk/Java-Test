5f605fc1eb48abdd97a3e5525a85d06a339c6468
Gasparsk <143566536+Gasparsk@users.noreply.github.com>
2024-08-20 16:34:55 -0300

    Update AT08.java

--- AT08.java
+++ AT08.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class AT08 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
 
         Scanner sc = new Scanner(System.in);
         float media = 0;
