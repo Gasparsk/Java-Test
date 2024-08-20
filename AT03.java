7f112dcc8882f402171393f1335ee3a24d42b076
Gasparsk <143566536+Gasparsk@users.noreply.github.com>
2024-08-20 15:14:01 -0300

    Update and rename AT-02.2.java to AT03.java

--- AT03.java
+++ AT03.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.mavenproject3;
+package com.mycompany.AT03;
 
 import java.util.Scanner;
 
@@ -9,7 +9,7 @@ import java.util.Scanner;
  *
  * @author aluno.den
  */
-public class Mavenproject3 {
+public class AT03 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
